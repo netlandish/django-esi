07818060bf4f7891ecb107ea16ff51e4200acf55
Cyril <cyrilrbt@gmail.com>
2014-01-14 17:46:32 -0500

    More cleanup

--- esi/urls.py
+++ esi/urls.py
@@ -13,14 +13,15 @@ example URL:
   /esi/news/story/12345/300/news/story_detail.html
 
 """
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('esi.views',
-    url(r'^(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/(?P<object_id>\d+)/$', 'esi', name='esi'),
-    url(r'^(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/(?P<object_id>\d+)/(?P<timeout>\d+)/$', 'esi', name='esi'),
-    url(r'^(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/(?P<object_id>\d+)/(?P<timeout>\d+)/(?P<template>[\w\-\/\.]+)/$', 'esi', name='esi'),
-    #url(r'^list/(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/(?P<object_id>\d+)/$', 'esi', name='esi_list'),
-    #url(r'^list/(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/(?P<object_id>\d+)/(?P<timeout>\d+)/$', 'esi', name='esi_list'),
-    # this url and the one above are exactly the same, and the url resolver doesn't understand this....  TIME FOR REFACTORING!
-    #url(r'^list/(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/(?P<object_id>\d+)/(?P<timeout>\d+)/(?P<template_dir>[\w\-\/\.]+)/$', 'esi', name='esi_list'),
+urlpatterns = patterns(
+    'esi.views',
+    url(r'^(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/'
+        '(?P<object_id>\d+)/$', 'esi', name='esi'),
+    url(r'^(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/'
+        '(?P<object_id>\d+)/(?P<timeout>\d+)/$', 'esi', name='esi'),
+    url(r'^(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/'
+        '(?P<object_id>\d+)/(?P<timeout>\d+)/(?P<template>[\w\-\/\.]+)/$',
+        'esi', name='esi'),
 )
